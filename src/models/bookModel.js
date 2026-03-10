cb86371e828253e6c1a4333c0153876b15ba8b3e
Belenair <123185003+Belenair@users.noreply.github.com>
2026-03-10 01:12:55 +0100

    Random message 2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,34 +1,89 @@
 const { v4: uuid } = require("uuid");
 
 let books = [
-    {
-        id: uuid(),
-        title: 'Mor ho!',
-        author: 'Samo Chalupka',
-        genre: 'Poezia',
-        publishedYear: '1864',
-        summary: 'Epicka basen o odvahe a hrdinstve'
-    },
+  {
+    id: uuid(),
+    title: "First movie",
+    author: "Samo Chalupka",
+    genre: "Poezia",
+    publishedYear: "1864",
+    summary: "Epicka basen o odvahe a hrdinstve",
+  },
+  {
+    id: uuid(),
+    title: "Secound book",
+    author: "Samo Chalupka2",
+    genre: "Drama",
+    publishedYear: "1964",
+    summary: "Epicka basen o odvahe",
+  },
+  {
+    id: uuid(),
+    title: "Third movie",
+    author: "Samo Chalupka3",
+    genre: "Linguistika",
+    publishedYear: "2064",
+    summary: "Epicka basen",
+  },
+  {
+    id: uuid(),
+    title: "Forth movie",
+    author: "Samo Chalupka4",
+    genre: "Kucharske recepty",
+    publishedYear: "2164",
+    summary: "Epicka",
+  },
+  {
+    id: uuid(),
+    title: "Fifth movie",
+    author: "Samo Chalupka5",
+    genre: "Psychologia",
+    publishedYear: "1764",
+    summary: "",
+  }
 ];
 
 const getAllBooks = () => {
-    return books;
+  return books;
 };
 
 const getBookById = (id) => {
-    return books.find()
-}
+  return books.find(book => book.id === id);
+};
 
 const createBook = ({ title, author, genre, publishedYear, summary }) => {
-
+  const newBook = { id:uuid(), title, author, genre, publishedYear, summary};
+  books.push(newBook);
+  return newBook;
 };
 
 const updateBook = (id, { title, author, genre, publishedYear, summary }) => {
+  const book = getBookById(id);
+  if (!book) {
+    return { ok: false, message: "This request cannot be competed!"};
+  }
+  if(title) book.title = title;
+  if(author) book.author = author;
+  if(genre) book.genre = genre;
+  if(publishedYear) book.publishedYear = publisherYear;
+  if(summary) book.summary = summary;
 
+  return { ok: true, message: "Book updated!", data: book};
 };
 
 const deleteBook = (id) => {
-
+  const bookIndex = books.findIndex(book => book.id === id);
+  if (bookIndex === -1) {
+    return false;
+  }
+  books.splice(bookIndex, 1);
+  return true;
 };
 
-module.exports = { getAllBooks, getBookById, creteBook, updateBook, deleteBook}
+module.exports = {
+  getAllBooks,
+  getBookById,
+  creteBook,
+  updateBook,
+  deleteBook,
+};
